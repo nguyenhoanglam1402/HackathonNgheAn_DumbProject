5ea740dae6112aa39ba7b0c9ed18e34ad4dc62e5
Vo Thanh <tthanhh410@gmail.com>
2021-09-07 01:40:14 +0700

    feat: Create footer

---
+++ src/components/common/FooterComponent/index.jsx
@@ -0,0 +1,12 @@
+import './Style.css'
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FooterComponent;
