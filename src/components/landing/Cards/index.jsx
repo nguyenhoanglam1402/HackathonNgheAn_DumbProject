3439941f56a0c2b696e20cb5d846a2bdff256d57
nguyenhoanglam1402 <nguyenhoanglam.emily@gmail.com>
2021-09-06 11:38:39 +0700

    fix(navigation bả): Fix position of NavBar

---
+++ src/components/landing/Cards/index.jsx
@@ -0,0 +1,26 @@
+import { React, useState } from "react";
+import Card from "./card";
+import "./style.css";
+import image from "assets/other/card/ngu-coc.png";
+
+const Cards = () => {
+  const [list, setList] = useState([
+    "Gạo",
+    "Rau củ",
+    "Trái cây",
+    "Ngũ cốc",
+    "Cà phê",
+  ]);
+
+  return (
+    <div className="card-block">
+      <div className="cards">
+        {list.map((item, index) => (
+          <Card image={image} name={item} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
