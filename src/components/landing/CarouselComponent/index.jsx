67413f68d2d7e3d7aed4ffd496eef8e21a532473
Vo Thanh <tthanhh410@gmail.com>
2021-09-07 01:26:44 +0700

    update: update homepage

--- src/components/landing/CarouselComponent/index.jsx
+++ src/components/landing/CarouselComponent/index.jsx
@@ -3,6 +3,7 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css"
 import "./Style.css"
+import { Link } from 'react-router-dom';
 import slides from './images';
 
 const CarouselComponent = () => {
@@ -10,8 +11,10 @@ const CarouselComponent = () => {
         dots: false,
         infinite: true,
         speed: 2000,
+        fade: true,
         autoplay: true,
-        autoplaySpeed: 800,
+        autoplaySpeed: 2500,
+        csssEase: "linear",
         draggable: false,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -21,12 +24,42 @@ const CarouselComponent = () => {
         <div className="slide-container">            
             <Slider {...settings}>
                 <div className="slides">
+                    <div className="slide-content">
+                        <div className = "text">
+                            Nông dân Bùi Thị Minh Nguyệt đang có 1 tấn lựu không hạt ngon như gái 18 muốn bán.
+                        </div>
+                        <button className="infor-link">                        
+                            <Link to="/#" className="link" >
+                                Thông tin chi tiết tại đây
+                            </Link>
+                        </button>
+                    </div>
                     <img className="slide" src={slides.img1}/>
                 </div>
                 <div className="slides">
+                    <div className="slide-content">
+                        <div className = "text">
+                            Nông dân Bùi Thị Minh Nguyệt đang có 1 tấn lựu không hạt ngon như gái 18 muốn bán.
+                        </div>
+                        <button className="infor-link">                        
+                            <Link to="/#" className="link" >
+                                Thông tin chi tiết tại đây
+                            </Link>
+                        </button>
+                    </div>
                     <img className="slide" src={slides.img2}/>
                 </div>
                 <div className="slides">
+                    <div className="slide-content">
+                        <div className = "text">
+                            Nông dân Bùi Thị Minh Nguyệt đang có 1 tấn lựu không hạt ngon như gái 18 muốn bán.
+                        </div>
+                        <button className="infor-link">                        
+                            <Link to="/#" className="link" >
+                                Thông tin chi tiết tại đây
+                            </Link>
+                        </button>
+                    </div>
                     <img className="slide" src={slides.img3}/>
                 </div>
             </Slider>
