ef96ec06b0d900a230f0b885c653c3e5ac402e7c
Vo Thanh <tthanhh410@gmail.com>
2021-09-06 11:37:26 +0700

    feat(CarouselComponent): Create carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
+import { CarouselComponent } from "components/landing";
 import { React } from "react";
 import { NavigaionBar } from "../../components/common";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="home-container">
       <NavigaionBar />
+      <CarouselComponent/>
     </div>
   );
 };
