a05268c5417d2d3ebdc77b4c7b9222cd169ebe44
Vo Thanh <tthanhh410@gmail.com>
2021-09-07 10:57:48 +0700

    add

---
+++ src/components/common/FooterComponent/index.jsx
@@ -0,0 +1,35 @@
+import './Style.css';
+import React from 'react';
+import logo from "assets/icons/logo.png";
+
+const FooterComponent = () => {
+    return (
+        <div className = "footer-container">
+            <div className="footer-row-above">
+                <div className="footer-logo-container">
+                    <img className="logo-footer-image" src={logo}/>
+                </div>
+                <div className = "footer-about-container">
+                    <h2>Description</h2>
+                    <p>aaaaaaaaaaaaaaaaaaaaaa</p>
+                </div>
+                <div className = "footer-product-container">
+                    <h2>Product</h2>
+                    <p>aaaaaaaaaaaaaaaaaaaaaa</p>
+                </div>
+                <div className = "footer-contact-container">
+                    <h2>Contact</h2>
+                    <p>aaaaaaaaaaaaaaaaaaaaaa</p>
+                </div>
+            </div>
+            <div className="footer-row-under">
+                <div className = "footer-contact-container">
+                    <h2>Contact</h2>
+                    <p>aaaaaaaaaaaaaaaaaaaaaa</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FooterComponent;
